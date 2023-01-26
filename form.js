d18420fea14050e1e39f92c1ec5aaee0cdb33309
aniket3765 <121852693+aniket3765@users.noreply.github.com>
2023-01-25 21:21:58 -0500

    Update form.js

--- form.js
+++ form.js
@@ -1,34 +1,25 @@
-const form = document.querySelector('#btn');
-const name= document.querySelector('#name');
-const email = document.querySelector('#email');
 
-document.getElementById("btn").addEventListener("click", onsubmit);
-function onsubmit(){
-    
-   if(name.value=='' || email.value==''){
-    alert('Enter your credintials!');
-   } 
-   else{
-    console.log("Name :"+name.value+"Email"+email.value);
-   }
-   
-}
 //DOM Manupulation----
 // console.log(document.URL);
 // console.log(document.title);
 // console.log(document.links);
- console.log(document.getElementById('form'));
+ //console.log(document.getElementById('form'));
 var a =document.getElementById('form');
 console.log(a.textContent)
 //form.style.backgroundColor ='green'
 li2.style.backgroundColor = 'green';
 form.style.fontweight = 'Bold';
 
-const collection = document.getElementsByClassName('li2');
+/*const collection = document.getElementsByClassName('li2');
 for (let i = 0; i < collection.length; i++) {
    collection[i].style.backgroundColor = "red";
  }
  const li = document.getElementsByTagName('li');
  li[1].style.backgroundColor='yellow'
 
-
+var odd = document.querySelectorAll('li:nth-child(odd)');
+var item = document.querySelectorAll(".list-group-item");
+item[1].style.color="green";
+for(var i =0;i<odd.length;i++){
+ odd[i].style.backgroundColor ='green';
+}*/
